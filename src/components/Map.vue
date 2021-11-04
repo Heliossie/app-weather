cf77354dcb30ec88715fa72f2f3130cf870e00c4
Heliossie <frousseau@gmx.fr>
2021-11-04 11:52:42 +0100

    start add map leaflet

---
+++ src/components/Map.vue
@@ -0,0 +1,21 @@
+<template>
+  <l-map ref="myMap" @ready="doSomethingOnReady()"></l-map>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.nextTick(() => {
+      this.$refs.myMap.mapObject.ANY_LEAFLET_MAP_METHOD();
+    });
+  },
+  methods: {
+    doSomethingOnReady() {
+      this.map = this.$refs.myMap.mapObject;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
